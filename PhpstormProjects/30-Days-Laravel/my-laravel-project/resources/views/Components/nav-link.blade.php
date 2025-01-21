8f85ec27c10cd9653f1d82c2986f4abb89b9b2f4
SenthVyra <88665212+SenthVyra@users.noreply.github.com>
2025-01-21 12:05:47 +0530

    initial commit

---
+++ PhpstormProjects/30-Days-Laravel/my-laravel-project/resources/views/Components/nav-link.blade.php
@@ -0,0 +1,8 @@
+@props(['active'])
+<a class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}"
+   aria-current="{{ request()->is('/') ? 'page' : 'false' }}">
+    {{$attributes}}
+    {{ $slot }}
+</a>
+
+
